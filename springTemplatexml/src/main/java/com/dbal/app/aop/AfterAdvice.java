f2930dd4ad04fe7e3076a3319b1fdce259ff1add
choi <chj5156@naver.com>
2020-12-28 11:07:54 +0900

    hh

---
+++ springTemplatexml/src/main/java/com/dbal/app/aop/AfterAdvice.java
@@ -0,0 +1,7 @@
+package com.dbal.app.aop;
+
+public class AfterAdvice {
+	public void afterLog() {
+		System.out.println("[공통 로그] after");
+	}
+}
---
+++ springTemplatexml/src/main/java/com/dbal/app/aop/LogAdvice.java
@@ -0,0 +1,7 @@
+package com.dbal.app.aop;
+
+public class LogAdvice {
+	public void printLog() {
+		System.out.println("[공통 로그] before");
+	}
+}
